d75355964a30cd9d7adfce1469c4cbda06ad2978
ellos <elliottmv0@gmail.com>
2026-06-22 16:56:27 -0700

    added some tabbing

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -8,49 +8,22 @@ makeOp op args = concat [codegen (args !! 0), " ", op, " ", codegen (args !! 1)]
 wrapParens :: String -> String
 wrapParens x = "(" ++ x ++ ")"
 
-showC :: LispVal -> String
-showC (Atom x) = x
-showC (Number x) = show x
-showC (String x) = "\"" ++ x ++ "\""
-showC (Bool x) = if x then "true" else "false"
-showC (List []) = ""
-showC (List (op:args)) = case (op) of
-  (Atom "+") -> makeOp "+" args
-  (Atom "-") -> makeOp "-" args
-  (Atom "*") -> makeOp "*" args
-  (Atom "/") -> makeOp "/" args
-
-  (Atom "=") -> makeOp "==" args
-  (Atom "!=") -> makeOp "!=" args
-
-  (Atom "add") -> makeOp "+" args
-  (Atom "subtract") -> makeOp "-" args
-  (Atom "multiply") -> makeOp "*" args
-  (Atom "divide") -> makeOp "/" args
-
-  (Atom "and") -> makeOp "&&" args
-  (Atom "or") -> makeOp "||" args
-  (Atom "not") -> makeOp "!" args
-  
-  (Atom "print") -> concat ["puts(", showC (args !! 0), ");"]
-  (Atom "defun") -> concat [showC (args !! 1), " ", showC (args !! 0), "(){\n", showC (args !! 2), "\n}\n"]
-  (Atom "type") -> (case (args !! 0) of
-                      (Atom "i4") -> "int"
-                      (Atom "i2") -> "short"
-                      (Atom "bool") -> "bool"
-                      )
-
-  (Atom "if") -> concat ["if (", showC (args !! 0), ") {", showC (args !! 1), "}"]
-
-  -- (Atom "include") -> includeFile (showC $ args !! 0)
-  
-  (List _) -> concat (map showC (op:args))
-  _ -> "" 
-
 showArgs :: LispVal -> String
-showArgs (List xs) = " " ++ concat (map showArgs xs)
-showArgs (Atom x) = x
+showArgs (List (x:xs)) = concat [" ", showHs x, concat (map showArgs xs)]
 showArgs _ = ""
+
+defunBody :: LispVal -> String
+defunBody (List xs) = concat $ map (addTab . showHs) xs
+
+defun :: [LispVal] -> String
+defun args = concat [showHs (args !! 0), showArgs (args !! 1), defunTail args]
+      where
+        defunTail args = case (showHs $ args !! 0) of
+                  "main" -> concat [" = do\n",  defunBody (args !! 2), "\n"]
+                  _ -> concat [" = ", showHs (args !! 2), "\n"]
+
+addTab :: String -> String
+addTab x = (take 4 $ repeat ' ') ++ x
 
 showHs :: LispVal -> String
 showHs (Atom x) = x
@@ -72,13 +45,13 @@ showHs (List (op:args)) = case (op) of
        (Atom "!") -> wrapParens $ makeOp "!" args
 
        (Atom "print") -> concat ["putStrLn ", showHs (args !! 0), "\n"]
-       (Atom "defun") -> concat [showHs (args !! 0), showArgs (args !! 1), " = " , showHs (args !! 2), "\n"]
+       (Atom "defun") -> defun args
 
        (Atom "if") -> concat ["if ", showHs (args !! 0), " then (", showHs (args !! 1), ") else (", showHs (args !! 2), ")"]
 
        (List _) -> concat (map showHs (op:args))
        (Atom _) -> concat ["(", showHs (op), " ", concat (map showHs args), ")"]
-       (Number _) -> showHs op
+       (Number _) -> showHs op       
 
 codegen :: LispVal -> String
 codegen = showHs
